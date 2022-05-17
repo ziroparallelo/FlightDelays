a2fe53b754cda9a33b082ab0e32f7ac6d62061ab
lepadmin <lepadmin@lephp4.cdsdom.polito.it>
2022-05-17 10:29:33 +0200

    aggiunto database

--- src/main/java/it/polito/tdp/extflightdelays/db/ConnectDB.java
+++ src/main/java/it/polito/tdp/extflightdelays/db/ConnectDB.java
@@ -17,7 +17,7 @@ public class ConnectDB {
 			HikariConfig config = new HikariConfig();
 			config.setJdbcUrl(jdbcURL);
 			config.setUsername("root");
-			config.setPassword("rootroot");
+			config.setPassword("root");
 			
 			// configurazione MySQL
 			config.addDataSourceProperty("cachePrepStmts", "true");
